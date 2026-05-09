fc10228ebc2bf2f791fb8a3b5d85b2383b2b4f5b
Amin Asylbyek <aminoqr12@gmail.com>
2026-05-09 18:50:35 +0200

    Time: 0 ms (100.00%) | Memory: 19.4 MB (7.09%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,9 +3,8 @@ class Solution:
         prefix = ""
         strs.sort()
         i = 0
-        last_i = len(strs) - 1
-        while i < len(strs[0]) and i < len(strs[last_i]):
-            if strs[0][i] == strs[last_i][i]:
+        while i < len(strs[0]) and i < len(strs[len(strs)-1]):
+            if strs[0][i] == strs[len(strs)-1][i]:
                 prefix += strs[0][i]
                 i+=1
             else:
