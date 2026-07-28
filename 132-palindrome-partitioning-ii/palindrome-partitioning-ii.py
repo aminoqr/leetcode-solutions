a74d12e4ec2db55d0418a0ebb29d1e1b9c92a5af
Amin Asylbyek <aminoqr12@gmail.com>
2026-07-28 17:00:54 +0200

    Time: 3158 ms (5.58%) | Memory: 20.4 MB (69.97%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -3,8 +3,8 @@ class Solution:
         n = len(s)
         memo = {}
 
-        def is_palindrome(substr: str) -> bool:
-            return substr == substr[::-1]
+        def is_palindrome(s: str) -> bool:
+            return s == s[::-1]
         
         def helper(i: int) -> int:
             if i == n:
@@ -14,7 +14,6 @@ class Solution:
                 return memo[i]
             
             min_cost = 10**99
-
             for j in range(i, n):
                 if is_palindrome(s[i:j+1]):
                     cost = 1 + helper(j+1)
@@ -23,5 +22,6 @@ class Solution:
             memo[i] = min_cost
             return min_cost
         
+
         return helper(0)
 
